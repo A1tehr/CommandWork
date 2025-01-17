e6e5794e9101f64ede031bdd45c98880ae7bed38
A1tehr- <villi500000@gmail.com>
2025-01-18 01:01:04 +0300

    init
    init

---
+++ TaskManagerProject/TaskManagerProject/Login_Registration/Greetings/LoginRegistration.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace TaskManagerProject.Login_Registration
+{
+    partial class LoginRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(302, 54);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(377, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Учет домашних заданий";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(416, 234);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "НАЧАТЬ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(218, 329);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(596, 19);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(215, 314);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Вход в систему...";
+            // 
+            // LoginRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.ClientSize = new System.Drawing.Size(1069, 409);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.MaximizeBox = false;
+            this.Name = "LoginRegistration";
+            this.Text = "LoginRegistration";
+            this.Load += new System.EventHandler(this.LoginRegistration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ TaskManagerProject/TaskManagerProject/Login_Registration/RegistrationAndLogin/Login.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TaskManagerProject.MainScreen;
+using TaskManagerProject.MetaData;
+using TaskManagerProject.Models;
+using TaskManagerProject.Repository;
+
+namespace TaskManagerProject.Login_Registration.RegistrationAndLogin
+{
+    public partial class Login : Form
+    {
+
+        JsonManager jsonManager;
+        string password = "";
+        string[] array;
+        private void SetUp()
+        {
+            base.CenterToParent();
+        }
+        public Login()
+        {
+            InitializeComponent();
+            SetUp();
+        }
+
+        private void Login_Load(object sender, EventArgs e)
+        {
+            base.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private bool SetUpName(TextBox Name) {
+            bool isSetUp = false;
+            string name = Name.Text.ToString().Trim();
+            array = new string[3];
+            int count = 0;
+            for(int i  = 0; i < name.Length; i++) {
+                if (name[i] != ' ') {
+                    array[count] = array[count] + name[i];
+                }
+                else
+                {
+                    count++;
+                    if (count == 3) return false;
+                    continue;                
+                }
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == null) {
+                    isSetUp = false;
+                    break;
+                }
+                else
+                {
+                    isSetUp = true;
+                }
+            }
+            return isSetUp;
+        }
+        private void SetUpPassword(TextBox passwordTBox)
+        {
+            password = passwordTBox.Text.ToString().Trim();
+        }
+
+        private async void EnterSystem(bool isExist, string f, string l, string t)
+        {
+            if (isExist)
+            {
+                SQLManager sqlManager = new SQLManager();
+                Student student = await sqlManager.GetStudent(f, l, t);
+                await JsonManager.CreateJson(student);
+
+                MainForm mainForm = new MainForm();
+                
+                mainForm.Show();
+                Login login = new Login();
+                base.Visible = false;
+            }
+            else
+            {
+                MessageBox.Show("Неверно введенные данные! Попробуйте снова!");
+            }
+        }
+
+        private async void CheckUser(string firstName, string lastName, string thirdName, string password)
+        {
+            SQLManager sqlManager = new SQLManager();
+            bool isExist = await sqlManager.CheckUserIfExist(firstName, lastName, thirdName, password);
+            EnterSystem(isExist, firstName, lastName, thirdName);
+        }
+
+        private void CheckIfEmpyField(out bool isField)
+        {
+            if (textBox1.Text == "" || textBox2.Text == "")
+            {
+                isField = false;
+            }
+            else
+            {
+                isField = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isFilled = false;
+            CheckIfEmpyField(out isFilled);
+            if (isFilled)
+            {
+                bool isSetUp = SetUpName(textBox1);
+                if (isSetUp)
+                {
+                    SetUpPassword(textBox2);
+                    CheckUser(array[1], array[0], array[2], password);
+                }
+                else
+                {
+                    MessageBox.Show("Введите верный формат полей 1) ФИО, 2) Пароль");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Все поля должны быть заполнены!");
+            }
+        }
+
+        private void ФИО_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Вход_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            
+            SecretForm secretForm = new SecretForm();
+            secretForm.Show();
+        }
+    }
+}
